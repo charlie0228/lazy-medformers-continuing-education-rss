c91674a1c9436ca592d34c38b8e597fcf166248f
AzureBlue <charlie800228@gmail.com>
2019-04-19 18:17:57 +0800

    hide google sheets

--- src/components/RssList.vue
+++ src/components/RssList.vue
@@ -37,8 +37,8 @@
         </select>
       </div>
       <div class="col d-flex justify-content-end align-items-center pr-3">
-        <a class="btn btn-danger" href="https://lazy-medformers-rss-to-google.herokuapp.com/"
-          role="button" target="_blank">匯出至Google Sheets</a>
+        <!-- <a class="btn btn-danger" href="https://lazy-medformers-rss-to-google.herokuapp.com/"
+          role="button" target="_blank">匯出至Google Sheets</a> -->
         <download-excel class="btn btn-info mx-3" :data="exportXLS()"
           name="活動列表.xls" style="cursor: pointer;">
           匯出EXCEL檔案
